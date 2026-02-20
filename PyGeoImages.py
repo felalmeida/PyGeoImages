02f6afbfa0e8341c40dfbb896a7ab90d14d2e52e
Felipe Almeida <fel.almeida@gmail.com>
2026-02-19 22:42:23 -0300

    Add PostgreSQL and RabbitMQ

--- PyGeoImages.py
+++ PyGeoImages.py
@@ -16,6 +16,8 @@ import turfpy.measurement
 import hashlib
 import pika
 import requests
+import dotenv
+import psycopg2
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
@@ -28,15 +30,18 @@ FieldDelim  = ','
 if not os.path.exists(MetaPath): os.makedirs(MetaPath)
 if not os.path.exists(LogPath): os.makedirs(LogPath)
 
+# MgOBJ_CONN = None
+PgSQL_CONN = None
+PgSQL_CURS = None
+Msg_Rabiit = None
+MsgChannelPublish = None
+RabiitMQ = None
+
 ExecutionId = ''
 ExecutionDt = ''
 jSources = {}
 gStatesInterestBBOX = []
 gCitiesInterestBBOX = []
-
-# MsgConnection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-# MsgChannelPublish = MsgConnection.channel()
-# MsgChannelPublish.queue_declare(queue='PyGeoImages', durable=True)
 
 
 def DictArrayToCsv(v_jArray, v_FieldDelim=','):
@@ -58,7 +63,41 @@ def DictArrayToCsv(v_jArray, v_FieldDelim=','):
 
 
 def EnvironmentSetup():
-    global ConfigPath, jSources, gStatesInterestBBOX, gCitiesInterestBBOX
+    global PgSQL_CONN, PgSQL_CURS, Msg_Rabiit, MsgChannelPublish, RabiitMQ, ConfigPath, jSources, gStatesInterestBBOX, gCitiesInterestBBOX
+
+    ### Env Variables
+    dotenv.load_dotenv()
+    PostgreSQL = {
+        'HOST': os.getenv('PgSQL_HOST', ''),
+        'PORT': int(os.getenv('PgSQL_PORT', 0)),
+        'USER': os.getenv('PgSQL_USER', ''),
+        'PASS': os.getenv('PgSQL_PASS', ''),
+        'NAME': os.getenv('PgSQL_NAME', ''),
+        'DEBUB': os.getenv('PgSQL_DEBUB', 'False') == "True"
+    }
+    RabiitMQ = {
+        'HOST': os.getenv('Msg_Rabiit_HOST', 'localhost'),
+        'PORT': int(os.getenv('Msg_Rabiit_PORT', '5672')),
+        'QUEUE': os.getenv('Msg_Rabiit_QUEUE', 'rabbimq_queue')
+    }
+
+    ### Postgre Database
+    PgSQL_CONN = psycopg2.connect (
+        host=PostgreSQL['HOST'],
+        port=PostgreSQL['PORT'],
+        user=PostgreSQL['USER'],
+        password=PostgreSQL['PASS'],
+        dbname=PostgreSQL['NAME']
+    )
+    PgSQL_CURS = PgSQL_CONN.cursor()
+
+    ### RabbitMQ
+    Msg_Rabiit = pika.BlockingConnection(pika.ConnectionParameters(
+        host=RabiitMQ['HOST'],
+        port=RabiitMQ['PORT']
+    ))
+    MsgChannelPublish = Msg_Rabiit.channel()
+    MsgChannelPublish.queue_declare(queue=RabiitMQ['QUEUE'], durable=True)
 
     ### Sources Config
     with open(ConfigPath+'Sources.json', 'r') as fConfigFile:
@@ -147,7 +186,7 @@ def GetPlanetaryComputer(v_Source=None, v_dtLoopStart=None, v_dtLoopEnd=None, v_
     planetarycomputer_catalog = pystac_client.Client.open(
         "https://planetarycomputer.microsoft.com/api/stac/v1",
         modifier=planetary_computer.sign_inplace)
-    planetarycomputer_catalog._stac_io.session = CreateRetrySession(retries=3, backoff_factor=1)
+    planetarycomputer_catalog._stac_io.session = CreateRetrySession()
 
     if (v_bUpdateCatallog):
         ### Organize Collections in Meta File Keeping Enable Status
@@ -204,7 +243,8 @@ def GetPlanetaryComputer(v_Source=None, v_dtLoopStart=None, v_dtLoopEnd=None, v_
             try:
                 CatSearch = planetarycomputer_catalog.search(collections=[CollectionId], bbox=gInterestBBOX['bbox'], datetime=dtRangeStr)
                 for CatSearchItem in CatSearch.items_as_dicts():
-                    CatSearchItem['_id'] = CatSearchItem['id']
+                    #CatSearchItem['_id'] = CatSearchItem['id']
+                    CatSearchItem['_id'] = str(hashlib.md5((ExecutionDt+CatSearchItem['id']).encode('UTF-8')).hexdigest())
                     CatSearchItem['_dt_update'] = datetime.datetime.now(datetime.UTC).astimezone().isoformat()
                     CatSearchItem['_ts_update'] = int(datetime.datetime.now(datetime.UTC).timestamp())
                     CatSearchItem['_query'] = {
@@ -213,6 +253,8 @@ def GetPlanetaryComputer(v_Source=None, v_dtLoopStart=None, v_dtLoopEnd=None, v_
                         'InterestBBOX_name':gInterestBBOX['name'],
                         'datetime':dtRangeStr
                     }
+                    CatSearchItem['_log_unique_id'] = str(hashlib.md5((CollectionId+str(gInterestBBOX['id'])+gInterestBBOX['name']+dtRangeStr+CatSearchItem['id']).encode('UTF-8')).hexdigest())
+
                     dtItem = datetime.datetime.fromisoformat(CatSearchItem['properties']['datetime'])
                     SavePath = os.path.realpath(MetaPath+CollectionId+'/'+dtItem.strftime("%Y%m%d")+'/'+str(gInterestBBOX['id']))
                     FileName = SavePath+'/'+CatSearchItem['id']+'.json'
@@ -228,14 +270,17 @@ def GetPlanetaryComputer(v_Source=None, v_dtLoopStart=None, v_dtLoopEnd=None, v_
 
                     ### Save Reference Log to Array
                     jLogData = {
+                        'LogUniqueId':CatSearchItem['_log_unique_id'],
                         'ExecutionId':ExecutionId,
                         'ExecutionDt':ExecutionDt,
                         'CollectionId':CollectionId,
                         'InterestBBOXId':gInterestBBOX['id'],
                         'InterestBBOXName':gInterestBBOX['name'],
-                        'dtRangeStr':dtRangeStr,
-                        'dtItem':dtItem.isoformat(),
-                        'FileName':CatSearchItem['_filename']
+                        'SearchRangeStartDt':v_dtLoopStart.astimezone().isoformat(),
+                        'SearchRangeEndDt':v_dtLoopEnd.astimezone().isoformat(),
+                        'MetaFileUniqueId':CatSearchItem['_id'],
+                        'MetaFileDt':dtItem.isoformat(),
+                        'MetaFileName':CatSearchItem['_filename']
                     }
                     LogDataArr.append(jLogData)
 
@@ -249,55 +294,147 @@ def GetPlanetaryComputer(v_Source=None, v_dtLoopStart=None, v_dtLoopEnd=None, v_
                 continue
 
     ### Save Log File (as CSV)
-    with open(os.path.realpath(LogPath+ExecutionId+'.csv'),'w') as fCsvLogFile:
+    LogFileName = LogPath+SourceData['SysName']+'_'+str(CollectionId)+'_'+str(ExecutionId)+'.csv'
+    with open(os.path.realpath(LogFileName),'w') as fCsvLogFile:
         fCsvLogFile.write(DictArrayToCsv(LogDataArr, FieldDelim))
 
 
-def ProcessPlanetaryComputer():
-    global ExecutionId, LogPath, MetaPath, FieldDelim #, MsgChannelPublish
+def ProcessPlanetaryComputer(v_Source=None):
+    global ExecutionId, LogPath, MetaPath, jSources, FieldDelim, PgSQL_CONN, PgSQL_CURS, MsgChannelPublish, RabiitMQ
 
-    with open(LogPath+ExecutionId+'.csv', 'r') as fCsvLogFile:
-        LogFile = fCsvLogFile.read()
+    SourceData = jSources[v_Source]
+    MetaFileName = os.path.realpath(MetaPath+SourceData['SysName']+'_'+'Collections.meta.json')
 
-    UniqFiles = []
-    CsvHEader = LogFile.split('\n')[0].split(FieldDelim)
-    for CsvLine in LogFile.split('\n')[1:]:
-        if (len(CsvLine) == 0):
-            continue
-        CsvItems = CsvLine.split(FieldDelim)
-        UniqFiles.append(CsvItems[-1])
-    UniqFiles = set(UniqFiles)
+    ### Get Updated and Enabled Collections
+    jCollections = []
+    with open(MetaFileName, 'r') as fConfigFile:
+        for Collections in json.load(fConfigFile):
+            if (Collections['Enabled'] == True):
+                jCollections.append(Collections)
 
-    ArrFilesToDownload = []
-    for MetaFile in UniqFiles:
-        with open(os.path.realpath(MetaFile), 'r') as fJsonMetaFIle:
-            jMetaFile = json.load(fJsonMetaFIle)
-            for jAssets in jMetaFile['assets']:
-                FileAssets = jMetaFile['assets'][jAssets]
-                if ('image' in FileAssets['type']):
-                    ArrFilesToDownload.append({
-                        'ExecutionId':ExecutionId,
-                        'MetaFile':MetaFile,
-                        'AssetName':jAssets,
-                        'AssetTitle':FileAssets['title'],
-                        'AssetType':FileAssets['type'],
-                        'HrefLink':FileAssets['href']
-                    })
+    for collection in jCollections:
+        CollectionId = collection['CollectionId']
 
-    ### Verify Existent Files Before RabbitMQ
+        LogUniqFiles = []
+        LogUniqItems = []
+        LogFileName = LogPath+SourceData['SysName']+'_'+str(CollectionId)+'_'+str(ExecutionId)+'.csv'
+        if os.path.isfile(LogFileName):
+            with open(LogFileName, 'r') as fCsvLogFile:
+                LogFile = fCsvLogFile.read()
 
-    for jDonFile in ArrFilesToDownload[:3]:
-        print(jDonFile)
-        # MsgChannelPublish.basic_publish (
-        #     exchange='',
-        #     routing_key='PyGeoImages',
-        #     body=json.dumps(jDonFile),
-        #     properties=pika.BasicProperties(delivery_mode=2)
-        # )
+            CsvHEader = LogFile.split('\n')[0].split(FieldDelim)
+            for CsvLine in LogFile.split('\n')[1:]:
+                if (len(CsvLine) == 0):
+                    continue
+                CsvItems = CsvLine.split(FieldDelim)
+                LogUniqItems.append(CsvItems[0])
+                LogUniqFiles.append(CsvItems[10])
+                PgSQL_Insert_Log = ("""
+                    INSERT INTO sat_images.metafiles_log (
+                        log_unique_id,
+                        execution_id,
+                        execution_dt,
+                        collection_id,
+                        interest_bbox_id,
+                        interest_bbox_name,
+                        search_range_start_dt,
+                        search_range_end_dt,
+                        meta_file_id,
+                        meta_file_dt,
+                        meta_file_name
+                    ) VALUES (
+                        '{log_unique_id}',
+                        '{execution_id}',
+                        to_timestamp('{execution_dt}','dd-mm-yyyy hh24:mi:ss'),
+                        '{collection_id}',
+                        {interest_bbox_id},
+                        '{interest_bbox_name}',
+                        to_timestamp('{search_range_start_dt}','dd-mm-yyyy hh24:mi:ss'),
+                        to_timestamp('{search_range_end_dt}','dd-mm-yyyy hh24:mi:ss'),
+                        '{meta_file_id}',
+                        to_timestamp('{meta_file_dt}','dd-mm-yyyy hh24:mi:ss'),
+                        '{meta_file_name}'
+                    );
+                    """.format(
+                        log_unique_id = CsvItems[0],
+                        execution_id = CsvItems[1],
+                        execution_dt = datetime.datetime.fromisoformat(CsvItems[2]).strftime("%d-%m-%Y %H:%M:%S"),
+                        collection_id = CsvItems[3],
+                        interest_bbox_id = CsvItems[4],
+                        interest_bbox_name = CsvItems[5].replace("'","''"),
+                        search_range_start_dt = datetime.datetime.fromisoformat(CsvItems[6]).strftime("%d-%m-%Y %H:%M:%S"),
+                        search_range_end_dt = datetime.datetime.fromisoformat(CsvItems[7]).strftime("%d-%m-%Y %H:%M:%S"),
+                        meta_file_id = CsvItems[8],
+                        meta_file_dt = datetime.datetime.fromisoformat(CsvItems[9]).strftime("%d-%m-%Y %H:%M:%S"),
+                        meta_file_name = CsvItems[10]
+                    ))
+
+                try:
+                    PgSQL_CURS.execute(PgSQL_Insert_Log)
+                except psycopg2.Error as e:
+                    pass #print(f"[DB ERROR] {e}")
+
+            LogUniqFiles = set(LogUniqFiles)
+            LogUniqItems = set(LogUniqItems)
+            PgSQL_CONN.commit()
+
+        ### Verify If Log File is in DB
+        PgSQL_Select_Log = ("""
+            SELECT COUNT(*) FROM sat_images.metafiles_log WHERE
+            execution_id='{execution_id}' AND collection_id='{collection_id}';
+            """.format(
+                execution_id = ExecutionId,
+                collection_id = CollectionId
+            ))
+        PgSQL_CURS.execute(PgSQL_Select_Log)
+        PgSQL_ROWS = PgSQL_CURS.fetchall()
+        PgSQL_Result = int(PgSQL_ROWS[0][0])
+
+        if ((PgSQL_Result == len(LogUniqItems)) and os.path.isfile(LogFileName)):
+            os.remove(LogFileName)
+
+        ### Verify If Log File is in DB
+        PgSQL_Select_Files_From_Log = ("""
+            SELECT DISTINCT meta_file_name FROM sat_images.metafiles_log WHERE
+            execution_id='{execution_id}' AND collection_id='{collection_id}';
+            """.format(
+                execution_id = ExecutionId,
+                collection_id = CollectionId
+            ))
+        PgSQL_CURS.execute(PgSQL_Select_Files_From_Log)
+        PgSQL_ROWS = PgSQL_CURS.fetchall()
+        PgSQL_Result = [DbItem[0] for DbItem in PgSQL_ROWS]
+        PgSQL_Result.sort()
+
+        ArrFilesToDownload = []
+        for MetaFile in PgSQL_Result:
+            with open(os.path.realpath(MetaFile), 'r') as fJsonMetaFIle:
+                jMetaFile = json.load(fJsonMetaFIle)
+                for jAssets in jMetaFile['assets']:
+                    FileAssets = jMetaFile['assets'][jAssets]
+                    if ('image' in FileAssets['type']):
+                        ArrFilesToDownload.append({
+                            'ExecutionId':ExecutionId,
+                            'MetaFile':MetaFile,
+                            'AssetName':jAssets,
+                            'AssetTitle':FileAssets['title'],
+                            'AssetType':FileAssets['type'],
+                            'HrefLink':FileAssets['href']
+                        })
+
+        ### Verify Existent Files Before RabbitMQ
+
+        for jDonFile in ArrFilesToDownload:
+            MsgChannelPublish.basic_publish (
+                exchange='',
+                routing_key=RabiitMQ['QUEUE'],
+                body=json.dumps(jDonFile),
+                properties=pika.BasicProperties(delivery_mode=2)
+            )
 
 
 def MainProcess():
-    global ExecutionId, ExecutionDt, jSources
+    global ExecutionId, ExecutionDt, jSources, PgSQL_CURS
 
     ExecutionDt = datetime.datetime.now(datetime.UTC).astimezone().isoformat()
     ExecutionId = str(hashlib.md5((ExecutionDt).encode('UTF-8')).hexdigest())
@@ -307,32 +444,56 @@ def MainProcess():
     dtWeekDay   = dtLoopEnd.date().isoweekday()
 
     bUpdateCatallog = False
-    if (dtWeekDay == 2):
+    if (dtWeekDay == 1):
         bUpdateCatallog = True
 
     # Just for DEV Tests
     dtLoopStart = dtLoopEnd.replace(hour=0, minute=0, second=0) - datetime.timedelta(days=7)
-    #ExecutionId = '3141ddf9f50ac558bceed87f07d5ca78'
+    ExecutionId = 'f86278350dd7c87e15b83a6627eb4f32'
+    #dtLoopStart = datetime.datetime.now().replace(hour=0,  minute=0,  second=0,  microsecond=0, day=24, month=8, year=2025)
+    #dtLoopEnd   = datetime.datetime.now().replace(hour=23, minute=59, second=59, microsecond=0, day=24, month=8, year=2025)
 
     EnvironmentSetup()
 
     for Source in jSources:
         if (jSources[Source]['SysName'] == 'PlanetaryComputer'):
-            GetPlanetaryComputer(Source, dtLoopStart, dtLoopEnd, bUpdateCatallog)
-            ProcessPlanetaryComputer()
+            # GetPlanetaryComputer(Source, dtLoopStart, dtLoopEnd, bUpdateCatallog)
+            ProcessPlanetaryComputer(Source)
 
 
 def main():
-    # global MsgConnection
+    global PgSQL_CONN, PgSQL_CURS, Msg_Rabiit
 
     try:
         MainProcess()
-        #MsgConnection.close()
-        sys.exit(0)
     except KeyboardInterrupt:
         print("Py Geo Images Interrupted!")
-        sys.exit(1)
+    finally:
+        if Msg_Rabiit:
+            Msg_Rabiit.close()
+        if PgSQL_CURS:
+            PgSQL_CURS.close()
+        if PgSQL_CONN:
+            PgSQL_CONN.close()
+        sys.exit(0)
 
 
 if __name__ == "__main__":
     main()
+
+'''
+DROP TABLE sat_images.metafiles_log;
+CREATE TABLE sat_images.metafiles_log (
+    log_unique_id           CHAR(32) PRIMARY KEY,
+    execution_id            CHAR(32),
+    execution_dt            TIMESTAMPTZ,
+    collection_id           VARCHAR(50),
+    interest_bbox_id        INTEGER,
+    interest_bbox_name      VARCHAR(100),
+    search_range_start_dt   TIMESTAMPTZ,
+    search_range_end_dt     TIMESTAMPTZ,
+    meta_file_id            CHAR(32),
+    meta_file_dt            TIMESTAMPTZ,
+    meta_file_name          VARCHAR(255)
+);
+'''
